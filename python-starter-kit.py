740572dd7ab9fb6fc5c974431c8a64b593978f04
orlando <216954379+knngboo@users.noreply.github.com>
2026-05-13 13:08:03 -0500

    first commit

---
+++ python-starter-kit.py
@@ -0,0 +1,20 @@
+print('Hello World!')
+name = input('Please enter your name: ')
+print(name)
+x = 42 + 206
+y = x / (0 + 1)
+print(x)
+print(y)
+# this is a comment
+# printing single quotes requires double quotes
+print("Hello there young one. Here's your quest for today.")
+first_name = "Kai"
+print(first_name)
+last_name = "Flamesong"
+print(first_name + " " + last_name)
+sentence = first_name + " " + last_name
+print(sentence)
+print(sentence.upper())
+print(sentence.lower())
+print(sentence.capitalize())
+print(sentence.count("a"))
