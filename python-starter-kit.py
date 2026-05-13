97b4a1b87d20a32dc0eb5e0fe1c70b16d5b14aef
orlando <216954379+knngboo@users.noreply.github.com>
2026-05-13 15:14:15 -0500

    working with strings

--- python-starter-kit.py
+++ python-starter-kit.py
@@ -1,20 +1,7 @@
-print('Hello World!')
-name = input('Please enter your name: ')
-print(name)
-x = 42 + 206
-y = x / (0 + 1)
-print(x)
-print(y)
-# this is a comment
-# printing single quotes requires double quotes
-print("Hello there young one. Here's your quest for today.")
-first_name = "Kai"
-print(first_name)
-last_name = "Flamesong"
-print(first_name + " " + last_name)
-sentence = first_name + " " + last_name
-print(sentence)
-print(sentence.upper())
-print(sentence.lower())
-print(sentence.capitalize())
-print(sentence.count("a"))
+first_name = "Max"
+last_name = "Boxer"
+
+first_name = input("Please enter your first name: ")
+last_name = input("Please enter your last name: ")
+full_name = f"Hello, {first_name.capitalize()} {last_name.capitalize()}"
+print(full_name)
