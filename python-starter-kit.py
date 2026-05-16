7ad6ed18b1785b4c681fce62421f62cbd5804bc4
orlando <216954379+knngboo@users.noreply.github.com>
2026-05-15 23:26:43 -0500

    retrieved dates using timedelta

--- python-starter-kit.py
+++ python-starter-kit.py
@@ -1,9 +1,15 @@
 # dates
 
 # import function from library
-from datetime import datetime
+from datetime import datetime, timedelta
 
 # call the datetime function
 date = datetime.now()
-
 print("The date is: " + str(date))
+
+# create the change in time
+time = timedelta(days=1)
+
+# calculate change in time
+date2 = date - time
+print("Yesterday was: " + str(date2))
