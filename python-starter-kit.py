d9261b67cb3071e1826ddc916d4417d3edd943fd
orlando <216954379+knngboo@users.noreply.github.com>
2026-05-16 20:09:34 -0500

    conditionals + math/personality quiz

--- python-starter-kit.py
+++ python-starter-kit.py
@@ -44,3 +44,61 @@ else:
     print("Something else went wrong.")
 finally:
     print("This is our cleanup code.")
+
+# conditionals
+print("\n==== CONDITIONALS ====")
+print("Welcome to your math/personality quiz. Please answer all questions and aim for an A+!\nYours truly, King Boo")
+x = input("Please choose an x value: ")
+y = input("Please choose an y value: ")
+z = input("Please choose an z value: ")
+pi = 3.14
+question1 = "What is " + str(x) + " * " + str(y) + "? "
+question2 = "Do you like Minecraft? "
+question3 = "What is (" + str(pi) + " - " + str(z) + ")/(" + str(x) + ")? "
+question4 = "Do you like computers? "
+question5 = "How many questions were in this quiz? "
+secret_question = "Hello, what's your name? "
+
+null = input("Please press enter to continue:")
+name = input(secret_question)
+print("Thank you for sharing! Next question!")
+answer1 = input(question1)
+answer2 = input(question2)
+answer3 = input(question3)
+answer4 = input(question4)
+answer5 = input(question5)
+null = input("AWESOME! Now let's see your results! Please press enter:")
+point1 = 0
+point2 = 0
+point3 = 0
+point4 = 0
+point5 = 0
+point6 = 0
+
+if float(answer1) == int(x) * int(y):
+    point1 = 1
+else:
+    pass
+
+if answer2 == "yes":
+    point2 = 1
+else:
+    pass
+
+if float(answer3) == (int(pi) - int(z)) / int(x):
+    point3 = 1
+else:
+    pass
+
+if answer4 == "yes":
+    point4 = 1
+else:
+    pass
+
+if int(answer5) == 6:
+    point5 = 1
+else:
+    pass
+   
+average = ((point1 + point2 + point3 + point4 + point5 + point6) / 6) * 100
+print(average)
